34f42bb836a19dfdee04d5c7145032da066ea7b1
Thananyaoso <132637353+Thananyaoso@users.noreply.github.com>
2023-07-07 15:59:00 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Stack-Assignment-AjMingmanas211-22-16/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstring>
+#include "stack.h"
+
+int main(int argc, char* argv[]) {
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      int copen = 0;
+      int cclose = 0;
+      std::string expression = argv[i];
+      bool equal = false;
+      Stack stack;
+
+      for (char c : expression) {
+        if (c == '(' || c == '[' || c == '{') {
+          stack.push(c);
+          copen++;
+        } else if (c == ')' || c == ']' || c == '}') {
+          char open = stack.get_top();
+          if (stack.empt()) {
+            if ((open == '(' && c == ')') ||
+                (open == '[' && c == ']') ||
+                (open == '{' && c == '}')) {
+              cclose++;
+              continue;
+            }
+          }
+          stack.pop();
+          copen--;
+        }
+      }
+
+      if (stack.empt() && copen == 0 && cclose == 0) {
+        equal = true;
+      }
+
+      if (equal) {
+        std::cout << "Match" << std::endl;
+      } else if (copen > cclose) {
+        std::cout << "Too many open" << std::endl;
+      } else if (cclose > copen) {
+        std::cout << "Too many close" << std::endl;
+      } else {
+      }
+    }
+  }
+
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@Stack-Assignment-AjMingmanas211-22-16/stack.h
@@ -0,0 +1,69 @@
+#ifndef stack_h
+#define  stack_h
+#include "node.h"
+
+class Stack{
+private:
+	 NodePtr top;
+	 int size;
+public:
+    Stack(NodePtr = NULL);
+    ~Stack();
+    char pop();
+    void push(char);
+    char get_top();
+};
+
+Stack::Stack(NodePtr t){
+  if(t) {
+    top=t;
+    size=1;
+  }
+ else{
+   top=NULL;
+	 size=0;
+   }
+}
+void Stack::push(char x){
+   NodePtr newnode=new NODE(x);
+  //1
+  if(newnode){
+      newnode->set_next(top);
+      top = newnode;
+      size++;
+   }
+  else cout<<"No memory left for new nodes"<<endl;
+		 // Left missing for exercises…
+}
+char Stack::pop(){
+ 	  NodePtr t=top;
+		int value;		
+	 if(t){
+     value=t->get_value();
+     top=t->get_next();
+	   delete t;
+     size--;
+     return value;
+     }
+    
+	 cout<<"Empty stack"<<endl;
+   return 0;
+	}
+Stack::~Stack(){
+   cout<<"Clearing all stacks"<<endl;
+  	int i;
+  NodePtr t=top;
+for(i=0;i<size;i++){
+     NodePtr t = top;
+     top = top->get_next();
+     delete t;
+   	
+  }
+}
+
+char Stack::get_top(){
+  return top->get_value();
+}
+
+
+#endif
